7191e44ff8ca7a2fc1bb05998906d6cc5e4cc742
Nathaniel Wright <nathan@nathanieljwright.com>
2023-04-13 18:22:52 -0700

    Added basic styling

--- emacs/init.el
+++ emacs/init.el
@@ -1,12 +1,33 @@
 ;; ------------------------------------------------------------------------- ;;
-;;		         Package Initialization                              ;;
+;;		          Package Initialization                             ;;
 ;; ------------------------------------------------------------------------- ;;
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+;; Set up use package to automatically install packages that are requested
+;; later in this document
 (require 'use-package)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; Auto update packages and delete older versions
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 ;; ------------------------------------------------------------------------- ;;
 ;;			         Style                                       ;;
 ;; ------------------------------------------------------------------------- ;;
 (set-frame-font "Cascadia Code 12")
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(use-package doom-themes
+  :config
+  (load-theme 'doom-xcode t))
+
+
+
