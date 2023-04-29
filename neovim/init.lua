7e84d52b4101ba8be6f282026bf8404d3c10aa93
Nathaniel Wright <nathan@nathanieljwright.com>
2023-04-29 08:03:34 -0700

    Followed a guide to set up lazy + install colorscheme

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,5 +1,3 @@
-vim.cmd("colorscheme desert")
-
 vim.opt.smarttab = true
 vim.opt.showmatch = true
 vim.opt.ruler = true
@@ -19,3 +17,39 @@ vim.opt.hlsearch = false
 
 vim.g.mapleader = ','
 
+local lazy = {}
+
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  -- You can "comment out" the line below after lazy.nvim is installed
+  lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+  require('lazy').setup(plugins, lazy.opts)
+end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+lazy.setup({
+  ---
+  -- List of plugins
+  ---
+  {'folke/tokyonight.nvim'},
+})
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight')
