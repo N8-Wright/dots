d92f77769eb97903d2268cb0d5f8f8c0571e7015
Nathaniel Wright <nathan@nathanieljwright.com>
2023-04-29 07:49:03 -0700

    Started nvim config

---
+++ neovim/init.lua
@@ -0,0 +1,13 @@
+vim.cmd("colorscheme desert")
+
+vim.opt.smarttab = true
+vim.opt.showmatch = true
+vim.opt.ruler = true
+
+-- Set tabs to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Always indent using spaces
+vim.opt.expandtab = true 
+
