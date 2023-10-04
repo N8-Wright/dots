06e206f138cd2a9cf7b4fd33c93c7f1f437dfb26
Nathaniel Wright <nathan@nathanieljwright.com>
2023-10-04 08:38:23 -0700

    Added guid generate

--- .emacs.d/lisp/util.el
+++ .emacs.d/lisp/util.el
@@ -7,3 +7,13 @@
           (split-string (buffer-substring start end)) ", ")))
     (delete-region start end)
     (insert insertion)))
+
+
+(defun n/generate-guid ()
+  "Use some system native method of generating a guid"
+  (interactive)
+  (pcase system-type
+    ('windows-nt
+     (shell-command "powershell.exe -Command [guid]::NewGuid().toString()" t))
+    (_
+     (shell-command "uuidgen" t))))
