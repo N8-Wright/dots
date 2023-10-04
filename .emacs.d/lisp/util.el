0ed2cc969af7c1772191abb0b2696d1d125f14b7
Nathaniel Wright <nathan@nathanieljwright.com>
2023-10-04 08:28:53 -0700

    Added back arrayify function

---
+++ .emacs.d/lisp/util.el
@@ -0,0 +1,9 @@
+(defun arrayify (start end quote)
+  "Turn strings on newlines into a QUOTEd, comma-separated one-liner."
+  (interactive "r\nMQuote: ")
+  (let ((insertion
+         (mapconcat
+          (lambda (x) (format "%s%s%s" quote x quote))
+          (split-string (buffer-substring start end)) ", ")))
+    (delete-region start end)
+    (insert insertion)))
