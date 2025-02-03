70c15aa77b95458758c0c4deb3ee523eff8703a5
Nathaniel Wright <nathan@nathanieljwright.com>
2025-02-03 08:11:21 -0800

    Added deploy script for windows

---
+++ deploy.ps1
@@ -0,0 +1,3 @@
+New-Item -path ~\AppData\Roaming\.emacs -itemType SymbolicLink -target .emacs
+New-Item -path ~\AppData\Roaming\.emacs.nw -itemType SymbolicLink -target .emacs.nw
+New-Item -path ~\AppData\Roaming\.emacs-custom.el -itemType SymbolicLink -target .emacs-custom.el
