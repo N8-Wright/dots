7c8d3d42bbbd82a7b1747581cda326c50c5ca5fc
Nathaniel Wright <nathan@nathanieljwright.com>
2025-02-03 08:11:05 -0800

    Customized

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,11 +3,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes '(leuven-dark))
+ '(global-display-line-numbers-mode t)
+ '(indent-tabs-mode t)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(ns-alternate-modifier 'super)
+ '(ns-command-modifier 'meta)
+ '(ns-right-alternate-modifier 'none)
+ '(ns-right-command-modifier 'super)
  '(package-selected-packages
-   '(org-roam company dap-mode clang-format magit which-key multiple-cursors ivy)))
+   '(company dap-mode org-roam clang-format magit which-key multiple-cursors ivy))
+ '(scroll-bar-mode nil)
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(vc-follow-symlinks t nil nil "Since my config is symlinked into the home directory, setting this to follow links automatically is very useful."))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "nil" :slant normal :weight regular :height 160 :width normal)))))
