a6d49af59ed61c34e7d5f3f4f9c7b82ddeedfec2
Nathaniel Wright <nathan@nathanieljwright.com>
2025-03-04 09:21:52 -0800

    changed theme to light

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-enabled-themes '(leuven-dark))
+ '(custom-enabled-themes '(whiteboard))
  '(global-display-line-numbers-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
