62656f3d0f09668b99a0925e1c7ebda107ce3962
Nathaniel Wright <nathan@nathanieljwright.com>
2025-09-25 09:09:30 -0700

    Scroll till first error in compilation buffer

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(compilation-scroll-output 'first-error)
  '(custom-enabled-themes '(whiteboard))
  '(global-display-line-numbers-mode t)
  '(global-tab-line-mode t)
